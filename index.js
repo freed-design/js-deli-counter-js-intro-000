48e35a264c8736bef3a51b0318f5cc226d8c66b5
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-17 02:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ var takeANumber = (katzDeliLine, newName) => {
   katzDeliLine.push(newName)
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
 }
+
+var nowServing = () => {
+  if (katzDeliLine.length = 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return `It is now ${katzDeliLine[0]}'s turn to be served!`
+  }
+}
