1a0b95930a7e12c501254ab561721945a1c54f61
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-17 02:13:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ var nowServing = (katzDeliLine) => {
   if (katzDeliLine.length = 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `It is now ${katzDeliLine[0]}'s turn to be served!`
+    return `Currently serving ${katzDeliLine[0]}.`
   }
 }
